5649a3e4dc3ca587742ba276d72c74fb03f5097b
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-03-05 14:29:00 +0300

    changed src/init.js on step#1

--- src/init.js
+++ src/init.js
@@ -1,28 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import _ from 'lodash';
+import init from './index.js';
 
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = `<div class="container">
-      <div class="row">
-        <div class="col-6">
-          <form class="form-inline">
-            <div class="form-group">
-              <label>Capital</label>
-              <input class="ml-2 form-control" type="text" data-autocomplete-name="capital" data-autocomplete="/capitals.json">
-            </div>
-          </form>
-        </div>
-        <div class="col-6">
-          <ul data-autocomplete-name="capital">
-            <li>Nothing</li>
-          </ul>
-        </div>
-      </div>
-    </div>`;
-
-  return element;
-}
-
-document.body.appendChild(component());
+init();
