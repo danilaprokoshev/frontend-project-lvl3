558d68fd28c7c605382702d493e3c42fbb376abb
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-03-10 07:56:08 +0300

    updated view/watchers.js on step#3

--- view/watchers.js
+++ view/watchers.js
@@ -1,4 +1,5 @@
 import onChange from 'on-change';
+import i18n from 'i18next';
 
 const renderErrors = (body, errors) => { // TODO: maybe error, not errors
   const inputEl = body.querySelector('input');
@@ -23,7 +24,7 @@ const renderFeeds = (body, watchedState) => {
   const feedsCol = document.createElement('div');
   feedsCol.classList.add('col-md-10', 'col-lg-8', 'mx-auto', 'feeds');
   const feedsH2 = document.createElement('h2');
-  feedsH2.textContent = 'Фиды';
+  feedsH2.textContent = i18n.t('feeds');
   feedsCol.appendChild(feedsH2);
   const feedsUl = document.createElement('ul');
   feedsUl.classList.add('list-group', 'mb-5');
@@ -42,7 +43,7 @@ const renderFeeds = (body, watchedState) => {
   const postsCol = document.createElement('div');
   postsCol.classList.add('col-md-10', 'col-lg-8', 'mx-auto', 'posts');
   const postsH2 = document.createElement('h2');
-  postsH2.textContent = 'Посты';
+  postsH2.textContent = i18n.t('posts');
   postsCol.appendChild(postsH2);
   const postsUl = document.createElement('ul');
   postsUl.classList.add('list-group');
