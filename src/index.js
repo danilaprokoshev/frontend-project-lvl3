11006a431d3fe6f5a61a37e51f89f53669254a89
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-03-10 16:04:35 +0300

    updated src/index.js on step#3

--- src/index.js
+++ src/index.js
@@ -8,31 +8,6 @@ import watchState from '../view/watchers.js';
 import en from './locales/en.js';
 import ru from './locales/ru.js';
 
-const schema = yup.object().shape({
-  url: yup.string().required('Fill out this field').url('Ссылка должна быть валидным URL'),
-});
-
-const validate = (field) => {
-  try {
-    schema.validateSync(field, { abortEarly: false });
-    return {};
-  } catch (e) {
-    return _.keyBy(e.inner, 'path');
-  }
-};
-
-const checkInFeedList = (watchedState) => {
-  if (!watchedState.form.feedsList.includes(watchedState.form.field.url)) {
-    watchedState.form.feedsList.unshift(watchedState.form.field.url);
-    return {};
-  }
-  return {
-    url: {
-      message: 'RSS уже добавлен',
-    },
-  };
-};
-
 export default () => {
   const defaultLanguage = 'ru';
   i18n.init({
@@ -45,6 +20,38 @@ export default () => {
   }).then(() => {
     // TODO: что сделать в коллбеке...
   });
+
+  yup.setLocale({
+    string: {
+      required: i18n.t('form.validation.empty_field'),
+      url: i18n.t('form.validation.invalid_url'),
+    },
+  });
+
+  const schema = yup.object().shape({
+    url: yup.string().required().url(),
+  });
+
+  const validate = (field) => {
+    try {
+      schema.validateSync(field, { abortEarly: false });
+      return {};
+    } catch (e) {
+      return _.keyBy(e.inner, 'path');
+    }
+  };
+
+  const checkInFeedList = (watchedState) => {
+    if (!watchedState.form.feedsList.includes(watchedState.form.field.url)) {
+      watchedState.form.feedsList.unshift(watchedState.form.field.url);
+      return {};
+    }
+    return {
+      url: {
+        message: i18n.t('form.validation.already_added_rss'),
+      },
+    };
+  };
 
   const state = {
     form: {
@@ -62,7 +69,7 @@ export default () => {
   };
 
   const container = document.getElementById('point'); // TODO: вынести формирование начальных элекментов в отдельный модуль и использовать при инициализации приложения
-  container.classList.add('container-fluid', 'bg-dark', 'p-5');
+  container.classList.add('container-fluid', 'bg-dark', 'p-5'); // TODO: разобраться с элементами Bootstrap
 
   const row = document.createElement('div');
   row.classList.add('row');
@@ -74,7 +81,7 @@ export default () => {
   form.classList.add('rss-form');
 
   form.innerHTML = `<div class="row">
-  <div class="col-10">
+  <div class="col-8">
     <input autofocus="" required name="url" aria-label="url" class="form-control form-control-lg w-100" placeholder="ссылка RSS">
   </div>
   <div class="col-auto">
@@ -103,6 +110,7 @@ export default () => {
     const formData = new FormData(e.target);
     watchedState.form.field = Object.fromEntries(formData);
     updateValidationState();
+    console.log(watchedState);
     if (watchedState.form.valid) {
       const url = new URL(watchedState.form.field.url);
       axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`)
