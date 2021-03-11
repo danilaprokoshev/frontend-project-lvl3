81f29f324cf4ead39f075b400c52fbb7f80752ae
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-03-11 20:21:02 +0300

    updated src/parser.js on step#4

--- src/parser.js
+++ src/parser.js
@@ -4,14 +4,16 @@ export default (xml) => {
   const title = doc.querySelector('title');
   const description = doc.querySelector('description');
   const items = doc.getElementsByTagName('item');
-  const itemsContent = Array.from(items)
+  const posts = Array.from(items)
     .map((item) => ({
       title: item.querySelector('title').textContent,
       link: item.querySelector('link').textContent,
     }));
   return {
-    title: title.textContent,
-    description: description.textContent,
-    items: itemsContent,
+    feed: {
+      title: title.textContent,
+      description: description.textContent,
+    },
+    posts,
   };
 };
