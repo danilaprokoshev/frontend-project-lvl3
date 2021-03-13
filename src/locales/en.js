05938e31c74805e3903980b3363f0285b6bff225
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-03-14 00:31:54 +0300

    updated locales on step#5

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,6 +1,12 @@
 export default {
   translation: {
-    add: 'Add',
+    title: {
+      main_title: 'RSS aggregator',
+      trigger: 'Let\'s start reading RSS today! It\'s easy and pretty.',
+      placeholder: 'RSS URL',
+      add: 'Add',
+      example: 'e.g.',
+    },
     feeds: 'Feeds',
     posts: 'Posts',
     view: 'View',
@@ -9,7 +15,15 @@ export default {
         empty_field: 'Fill out this field',
         invalid_url: 'url must be valid URL',
         already_added_rss: 'RSS is already added',
+        invalid_rss: 'Origin doesn\'t include valid RSS',
       },
+      network_error: 'Network error',
+      success_feedback: 'RSS is successfully loaded',
     },
+    modal: {
+      read: 'Read article',
+      close: 'Close',
+    },
+    created: 'created by',
   },
 };
