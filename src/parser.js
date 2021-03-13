f150091dc26834b0c71f9c23798aa38fe88ec09c
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-03-14 02:17:00 +0300

    updated src/parser.js on check#0

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,6 @@
 export default (xml) => {
   const domparser = new DOMParser();
-  const doc = domparser.parseFromString(xml, 'application/xhtml+xml');
+  const doc = domparser.parseFromString(xml, 'text/xml');
   const title = doc.querySelector('title');
   const description = doc.querySelector('description');
   const items = doc.getElementsByTagName('item');
