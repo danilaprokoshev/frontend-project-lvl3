ac9eb8d008921563ab3dae00e562a7ae963a9b20
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-03-20 11:26:05 +0300

    updated src/init.js on check#1

--- src/init.js
+++ src/init.js
@@ -1,4 +1,10 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import fs from 'fs';
+import path from 'path';
 import runApp from './rssApp.js';
+
+const initHtml = fs.readFileSync(path.join(__dirname, '..', 'index.html')).toString();
+const parser = new DOMParser();
+parser.parseFromString(initHtml, 'text/html');
 
 runApp();
